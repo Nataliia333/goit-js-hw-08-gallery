172bb842b692e11307338d5da7be15e5465e2ed7
Nataliia333 <n.o.prykhodko@gmail.com>
2020-11-26 22:37:51 +0200

    создание разметки по массиву данных

---
+++ gallery.js
@@ -0,0 +1,14 @@
+import gallery from "./gallery-items.js";
+
+const refs = {
+    galleryList: document.querySelector('js.gallery'),
+    jsLightbox: document.querySelector('js.lightbox'),
+    lightboxImage: document.querySelector('.lightbox__image'),
+    lightboxBtn:  document.querySelector('button[data-action="close-lightbox"]'),
+    lightboxOverlay: document.querySelector('.lightbox__overlay'),
+};
+
+
+
+
+
