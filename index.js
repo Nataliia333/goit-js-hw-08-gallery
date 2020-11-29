2c0361c292f84e605e433eb15733fd6d05c684fb
Nataliia333 <n.o.prykhodko@gmail.com>
2020-11-29 18:30:44 +0200

    Открытие и закрытие modal, подмена значения атрибута src

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ import gallery from './gallery-items.js';
 const refs = {
     galleryList: document.querySelector('.js-gallery'),
     modal: document.querySelector('.lightbox'),
-    modalImg: document.querySelector('.lightbox__img'),
+    modalImg: document.querySelector('.lightbox__image'),
     btnCloseModal: document.querySelector('button[data-action="close-lightbox"]'),
     overlay: document.querySelector('.lightbox__overlay'),
 };
@@ -49,6 +49,7 @@ const galleryItems = gallery
 }
   
 refs.galleryList.insertAdjacentHTML('beforeend', galleryItems);
+
 //Реализация делегирования на галерее ul.js-gallery и получение url большого изображения
 
 refs.galleryList.addEventListener('click', onGalleryClick);
@@ -63,5 +64,30 @@ refs.galleryList.addEventListener('click', onGalleryClick);
     }
    
     openModal();
-    setModalImgSrc(bigImgRef);
+    changeModalImgSrc(bigImgRef);
 }
+
+// Открытие модального окна по клику на элементе галереи.
+
+// refs.modal.addEventListener('click', openModal);
+function openModal() {
+  refs.modal.classList.add('is-open');
+}
+
+// Подмена значения атрибута src элемента img.lightbox__image.
+
+function changeModalImgSrc(bigImgRef) {
+  refs.modalImg.src = bigImgRef;
+}
+
+// Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
+
+refs.btnCloseModal.addEventListener('click', closeModal);
+function closeModal() {
+refs.modal.classList.remove('is-open');
+
+// Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того, 
+// чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
+refs.modalImg.src = '';
+}
+
